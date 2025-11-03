2ddce06cca6f299b8ad98e4daf4bf8c7818f9c66
arn-c0de <arn-c0de@protonmail.com>
2025-11-03 21:37:44 +0100

    initial push

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from typing import Union, Optional
 # Add project root to path
 sys.path.insert(0, str(Path(__file__).parent))
 
-VERSION = "1.4.5"
+VERSION = "1.4.6"
 
 class CrawllamaException(Exception):
     """Custom exception for Crawllama application errors."""
